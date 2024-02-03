ebdc6f9ac75fae715363099dc26f7fba37400a5d
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2024-02-03 20:43:37 +0530

    Cpp-Data type

---
+++ Variable, Data Type/5_ex.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	
+	int RollNo = 10;
+	char letter = 'A';
+	string Name = "Vikee";
+	float time = 9.7;
+	double hight = 122.34566;
+
+	cout<<RollNo<<endl;
+	cout<<letter<<endl;
+	cout<<Name<<endl;
+	cout<<time<<endl;
+	cout<<hight<<endl;
+
+
+
+}
