ca4864e9d5984d1ff9243b370413c27212edb136
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2024-02-04 22:30:52 +0530

    Cpp operators

---
+++ Control_Statement/if_else if_else/switch_1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+	
+	int course = 2;
+
+	switch (course) {
+	
+	case 1:
+		std::cout<<"Java"<<std::endl;
+
+	case 2:
+		std::cout<<"Python"<<std::endl;
+
+	case 3:
+		std::cout<<"JvaScript"<<std::endl;
+
+	case 4:
+		std::cout<<"CPP"<<std::endl;
+
+	}
+
+	
+
+}
