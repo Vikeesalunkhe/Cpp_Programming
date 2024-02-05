31e21b050c020a5981cd5a4984c9a57885dc26f2
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2024-02-05 19:50:13 +0530

    Pattern for loop

---
+++ Control_Statement/Loop/For_Loop/pattern_5.cpp
@@ -0,0 +1,29 @@
+/*
+      *
+    * *
+  * * *
+* * * * 
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int num;
+	cout<<"Enter value of num : ";
+	cin>>num;
+
+	for (int i=1; i<=num; i++){
+	
+		for (int j=1; j<=num-i; j++) {
+		
+			cout<<" ";
+		}
+		for (int k=1; k<=i; k++){
+		
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+}
