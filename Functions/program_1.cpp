96c4684823e82073f01cb15c857f0c3dc7c07ffd
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2024-02-12 19:39:35 +0530

    Functions

---
+++ Functions/program_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void Demo(){                  //Fun return Type Fun name (Parameter type Parameters)
+	
+	cout<<"In This Demo function"<<endl;
+
+}
+
+int main(){
+	
+	Demo();          //Function Call
+	return 0;
+}
---
+++ Functions/program_2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int Add(int num1, int num2){
+	
+	int sum = num1 + num2;
+	//cout<<sum<<endl;
+	return sum;
+}
+
+
+int main(){
+	
+	int a=5;
+	int b=4;
+
+	cout<<Add(a,b)<<endl;
+
+
+	return 0;
+}
