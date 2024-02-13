3b6eae523c6dce1ce506565725ab75dbae154852
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2024-02-13 17:54:23 +0530

    Float_double_int

---
+++ Variable, Data Type/float.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+	cout<<sqrt(23)<<endl;
+	cout<<pow(2,3)<<endl;
+
+	int int_sqr = sqrt(23);
+	float float_sqr = sqrt(23);
+	double double_sqr = sqrt(23);
+
+	cout<<"in int "<<int_sqr<<endl;
+	cout<<"in float "<<float_sqr<<endl;
+	cout<<"in double "<<double_sqr<<endl;
+
+	return 0;
+
+
+}
