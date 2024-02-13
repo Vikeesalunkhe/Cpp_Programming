95678dbe784fae60fd1abc15a060f50419656c21
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2024-02-13 17:47:28 +0530

    return_in_Function

---
+++ Functions/program_6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+float Add(int num1, float num2){
+
+	float res = num1 + num2;
+
+	return res;
+}
+
+int main(){
+
+	int a=10;
+	float b=2.5;
+
+	cout<<Add(a, b)<<endl;
+	
+
+}
+
