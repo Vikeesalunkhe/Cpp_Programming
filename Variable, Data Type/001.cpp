a1f9417819a53edec906cbd07d148cd03ee5a653
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2026-03-27 19:38:16 +0530

    test

---
+++ Variable, Data Type/001.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    char name[20];
+    int num;
+    float fnum;
+
+    cout<<"Enter user input values : "<<"\n";
+
+    std::cout<<"this is second type  : "<<endl;
+
+    std::cout<<"Enter name : ";
+    cin>>name;
+    cout<<"name is : "<<name<<endl;
+
+    std::cout<<"Enter num : ";
+    std::cin>>num;
+    cout<<"Number is : "<<num<<endl;
+
+    cout<<"Enter float value : ";
+    cin>>fnum;
+    cout<<"Float value is : "<<fnum<<"\n";
+
+}
